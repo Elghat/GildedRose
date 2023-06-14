14de59abf95285b94b8e74a8b6ff5b47f13fb678
Gregoire Calonne <gregoire.calonne@live.be>
2023-06-14 16:19:38 +0200

    Begin refactor of UpdateQuality method
    Deep dive into the project to get the code in my head
     
    Then proceeding with some basic refactoring in the updateQuality method to make it more "readable" for me

--- GildedRose.cs
+++ GildedRose.cs
@@ -12,74 +12,61 @@ namespace csharp
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            foreach (var product in Items)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                //Ca ne sert à rien de continuer dans le if si c'est sulfura puisque l'item ne se dégrade pas 
+                if (product.Name == "Sulfuras, Hand of Ragnaros") { continue; }
+
+                if (product.Name != "Aged Brie" && product.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (Items[i].Quality > 0)
+                    if (product.Quality > 0)
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
+                        product.Quality -= 1;
                     }
                 }
                 else
                 {
-                    if (Items[i].Quality < 50)
+                    if (product.Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        product.Quality += 1;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (product.Name == "Backstage passes to a TAFKAL80ETC concert")
                         {
-                            if (Items[i].SellIn < 11)
+                            //Les billet de concert augmentent de qualité plus ils sont proche de la date de concert
+                            if (product.SellIn < 11 && product.Quality < 50)
                             {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
+                                product.Quality += 1;
                             }
 
-                            if (Items[i].SellIn < 6)
+                            if (product.SellIn < 6 && product.Quality < 50)
                             {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
+                                product.Quality += 1;
                             }
                         }
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
+                product.SellIn -= 1;
 
-                if (Items[i].SellIn < 0)
+                if (product.SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (product.Name != "Aged Brie")
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (product.Name != "Backstage passes to a TAFKAL80ETC concert" && product.Quality > 0)
                         {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
+                            product.Quality -= 1;
                         }
                         else
                         {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            //Si le concert est passé, la qualité du billet tombe à 0
+                            product.Quality = 0;
                         }
                     }
                     else
                     {
-                        if (Items[i].Quality < 50)
+                        if (product.Quality < 50)
                         {
-                            Items[i].Quality = Items[i].Quality + 1;
+                            product.Quality += 1;
                         }
                     }
                 }
