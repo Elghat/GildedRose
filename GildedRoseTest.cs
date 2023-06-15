a5b79c706fde80822bb96bef288c9469d0474cc8
Gregoire Calonne <gregoire.calonne@live.be>
2023-06-15 21:55:05 +0200

    New UpdateQuality Method
    Succesfuly implemented new UpdateQuality method
    
    Wrote Test and all pass

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -6,6 +6,47 @@ namespace csharp
     [TestClass]
     public class GildedRoseTest
     {
+        [TestMethod]
+        public void UpdateQualityTest()
+        {
+            //Arrange
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20},
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
 
+
+            //Act
+            var app = new GildedRose(items);
+            foreach (var listedItem in items)
+            {
+                app.UpdateQuality(listedItem);
+            }
+            
+
+            //Assert
+            Assert.AreEqual(9, items[0].SellIn);
+            Assert.AreEqual(19, items[0].Quality);
+
+            Assert.AreEqual(1, items[1].SellIn);
+            Assert.AreEqual(1, items[1].Quality);
+
+            Assert.AreEqual(4, items[2].SellIn);
+            Assert.AreEqual(6, items[2].Quality);
+
+            Assert.AreEqual(0, items[3].SellIn);
+            Assert.AreEqual(80, items[3].Quality);
+
+            Assert.AreEqual(14, items[4].SellIn);
+            Assert.AreEqual(21, items[4].Quality);
+            
+            Assert.AreEqual(2, items[5].SellIn);
+            Assert.AreEqual(4, items[5].Quality);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace csharp
         {
             Console.WriteLine("OMGHAI!");
 
-            IList<Item> Items = new List<Item>{
+            IList<Item> items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
@@ -41,19 +41,19 @@ namespace csharp
 				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
 
-            var app = new GildedRose(Items);
+            var app = new GildedRose(items);
 
 
             for (var i = 0; i < 31; i++)
             {
                 Console.WriteLine("-------- day " + i + " --------");
                 Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
+                foreach (var listedItem in items)
                 {
-                    System.Console.WriteLine(Items[j]);
+                    System.Console.WriteLine(listedItem);
+                    app.UpdateQuality(listedItem);
                 }
                 Console.WriteLine("");
-                app.UpdateQuality();
                 Console.ReadLine();
 
             }
