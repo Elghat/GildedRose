0d38e5a864791209d2e53c0c9c40bd6808978751
Gregoire Calonne <gregoire.calonne@live.be>
2023-06-15 09:15:51 +0200

    Begining "the real" refactor
    Implementing the future class that will be used in the new way of treating items 
    
    Will split into a branch for each new item added

---
+++ ItemBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Ici je vais avoir tous les comportement des items différents que uniquement UpdateQuality appellera
+
+///Todo : Implémenter le comportement de bases d'un item avec test
+///Todo : Implémenter les exceptions avec test à chaque fois
+namespace csharp
+{
+    internal class ItemBehavior
+    {
+
+    }
+}
---
+++ ItemBehaviorTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace csharp
+{
+    [TestFixture]
+    public class ItemBehaviorTest
+    {
+        [Test]
+        public void foo()
+        {
+            var product = new Item() { Name = "foo", SellIn = 0, Quality = 0 };
+            //Utiliser la métode default Behavior
+            
+            //Assert.AreEqual("fixme", Items[0].Name);
+        }
+    }
+}
